fab9b7aaee2aa074262e31aea7f1b3623dbab836
BrandonLopezDiaz <brandon.lopes.diaz@gmail.com>
2023-03-14 09:13:12 -0500

    Login

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="dashboard">
+      <h1>Esto es el dashboard</h1>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .dashboard {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
